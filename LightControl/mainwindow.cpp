bf6e8b4c5539ce135dbb1efd22f968177471fac5
RaCastanon <luisrcastan@gmail.com>
2017-10-17 15:07:43 -0500

    Serial Comm
    SeriialPort integrated

--- LightControl/mainwindow.cpp
+++ LightControl/mainwindow.cpp
@@ -2,14 +2,20 @@
 #include "ui_mainwindow.h"
 #include <opencv2/opencv.hpp>
 #include <QFileDialog>
+#include <QDebug>
+#include <QtSerialPort/QSerialPort>
 
 using namespace cv;
+
+//Global variables
+QSerialPort serial;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -17,15 +23,64 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+/* Function name: OpenImageButton()
+ * Developer:     Raul Castañon
+ * Details:       Function used to open and show an image
+ */
 void MainWindow::on_OpenImageButton_clicked()
 {
     QString inputImg = QFileDialog::getOpenFileName();
     Mat inputImage = imread(inputImg.toLatin1().data());
-    namedWindow("Selected Image", WINDOW_NORMAL);
-    imshow("Selected Image", inputImage);
+    //Verify if there is an image
+    if(!(inputImage.empty()))
+    {
+        //Show image
+        namedWindow("Selected Image", WINDOW_NORMAL);
+        imshow("Selected Image", inputImage);
+    }
 }
 
+/* Function name: closeApp()
+ * Developer:     Raul Castañon
+ * Details:       Close application
+ */
 void MainWindow::on_closeApp_clicked()
 {
     close();
+}
+
+/* Function name: openSerialPort()
+ * Developer:     Raul Castañon
+ * Details:       Configure and open the serialPort
+ */
+void MainWindow::on_openSerialPort_clicked()
+{
+    //Set configuration conditions
+    serial.setPortName("COM10");
+    serial.setBaudRate(QSerialPort::Baud9600);
+    serial.setDataBits(QSerialPort::Data8);
+    serial.setParity(QSerialPort::NoParity);
+    serial.setStopBits(QSerialPort::OneStop);
+    serial.setFlowControl(QSerialPort::NoFlowControl);
+    //Check if the port has been open
+    if(serial.open(QIODevice::ReadWrite))
+    {
+        qDebug(CONNECTION_SUCESSFUL);
+        serial.write("95\n");
+    }
+    //if not send error message
+    else
+    {
+        qDebug(SERIAL_COMM_ERROR);
+    }
+}
+
+/* Function name: closeSerialPort()
+ * Developer:     Raul Castañon
+ * Details:       Close serial port
+ */
+void MainWindow::on_closeSerialPort_clicked()
+{
+    serial.close();
+    qDebug(SERIAL_CLOSED);
 }
--- LightControl/mainwindow.h
+++ LightControl/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+#define SERIAL_COMM_ERROR "Error: could not open serial port"
+#define CONNECTION_SUCESSFUL "Serial communication sucessful"
+#define SERIAL_CLOSED "Serial port closed"
+
 namespace Ui {
 class MainWindow;
 }
@@ -19,6 +23,10 @@ private slots:
     void on_OpenImageButton_clicked();
 
     void on_closeApp_clicked();
+
+    void on_openSerialPort_clicked();
+
+    void on_closeSerialPort_clicked();
 
 private:
     Ui::MainWindow *ui;
